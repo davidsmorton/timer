ae329822c96fe664f66fe7f5638b68197292e898
davidsmorton <davidsmorton@gmail.com>
2020-09-23 05:22:08 +0000

    Broken code for timer, can't find how to limit conditions by string and NaN. Tried typof, isNaN, and abunch of other crazy stuff missing the simple answer.

---
+++ timer.js
@@ -0,0 +1,45 @@
+ 
+
+
+
+const timer = function(userInput) {
+  userInput =  process.argv.slice(2);
+  //console.log(userInput)
+  if (userInput < 1 ) {
+    return "nope";
+  } else if ("string") { // is there a better way?
+    return "not going to work"
+  } else if (NaN) {
+    return "try again"
+  } else {
+    for (let value of userInput)  {
+
+      let timInt = value * 1000;
+
+      //console.log(timInt);
+      setTimeout(() => {
+        process.stdout.write('\x07');
+      }, timInt);
+    }
+  }
+};
+console.log(timer());
+
+
+  
+  
+  
+//|| userInput < 1 || userInput === NaN
+  
+// let timInt = 0
+// process.stdout.write("\n");
+// for(let i = 0; i < sentence.length; i++) {
+//   setTimeout(() => {
+//       process.stdout.write(sentence[i] );
+//       }, timInt);
+//   timInt += 50
+// };
+
+// setTimeout(() => {
+//   process.stdout.write("\n")
+// }, timInt);
